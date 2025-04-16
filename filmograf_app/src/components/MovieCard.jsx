1dd195da699dd7aaa579489b5abbd746ed756e45
Natalia Novikova <novikova.nm@phystech.edu>
2025-04-16 20:24:55 +0300

    Добавила рамки вокруг фильмов но не отрегулировала по размеру

--- filmograf_app/src/components/MovieCard.jsx
+++ filmograf_app/src/components/MovieCard.jsx
@@ -1,15 +1,39 @@
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import {Button, Box, Text, HStack, Spacer, Flex} from '@chakra-ui/react';
 
+function MovieCard({movie, favorites, addToFavorites, removeFromFavorites}) { // Добавляем props
+    const isFavorite = favorites && favorites.includes(movie); // Проверка на null/undefined
 
-function MovieCard({ movie }) {
+    const handleAddToFavorites = () => {
+        addToFavorites(movie);
+    };
+
+    const handleRemoveFromFavorites = () => {
+        removeFromFavorites(movie);
+    };
+
     return (
-        <div className="movie-card">
-            <img src={movie.poster} alt={movie.title} />
-            <h3><Link to={`/movies/${movie.id}`}>{movie.title}</Link></h3>
-            <p>{movie.genre}</p>
-            <p>{movie.duration} мин.</p>
-            {/* Кнопка добавления в избранное */}
-        </div>
+        <Box>
+            <Flex direction="column" borderWidth="1px" borderRadius="md" overflow="hidden" p="4" boxShadow="md">
+                <img src={movie.poster} alt={movie.title}/>
+                <Box mt="2" flex="1">
+                    <Link to={`/movies/${movie.id}`}>
+                        <Text as="h3" fontWeight="semibold" fontSize="lg">{movie.title}</Text>
+                    </Link>
+                    <Text color="gray.500">{movie.genre}</Text>
+                    <Text color="gray.500">{movie.duration} мин.</Text>
+
+                </Box>
+            </Flex>
+            <Box mt="4">
+                {isFavorite ? (
+                    <Button colorScheme="red" size="xs" onClick={handleRemoveFromFavorites}>Удалить из
+                        избранного</Button>
+                ) : (
+                    <Button colorScheme="teal" size="xs" onClick={handleAddToFavorites}>Добавить в избранное</Button>
+                )}
+            </Box>
+        </Box>
     );
 }
 
